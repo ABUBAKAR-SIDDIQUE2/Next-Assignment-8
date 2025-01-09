da07d8bb4fb673964d8669b279a0bbaa818b01e4
ABUBAKAR-SIDDIQUE2 <a3325452033@gmail.com>
2025-01-09 09:18:00 +0500

    Added new Dataset

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -6,7 +6,7 @@ import { apiVersion, dataset, projectId } from '../env'
 export const client = createClient({
   token:process.env.NEXT_PUBLIC_SANITY_TOKEN,
   projectId:process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset:'production',
+  dataset:process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion:process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
